e63d089daaded00a6eea2c6e247d0b0faecfabda
parscaper <whynotparu@gmail.com>
2020-04-14 23:19:31 -0600

    Update DataText.java

--- src/Code/DataText.java
+++ src/Code/DataText.java
@@ -6,11 +6,11 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class DataText {
-	
+	//creates text file at specified location
 	public void createFile(String fileName, String directory) {
-		
+		//gets current working directory
 		String absolutePath = directory + File.separator + fileName;
-		
+		//Creates new file object and creates the file at location
 		try {
 			
 			File newFile = new File(absolutePath);
@@ -24,18 +24,19 @@ public class DataText {
 			}
 			
 		}
+		//catches location
 		catch (IOException e) {
 			System.out.print("Failed File Creation");
 		}
 	
 	}
 	
-	
+	//Updates text file with parameters
 	public void updateText(String fileName, String directory, String name, String ID){
 		
 		//directory to file and file name combined.
 		String absolutePath = directory + File.separator + fileName;
-		
+		//updates given file with parameters
 		try {
 			
 			FileWriter fileWriter = new FileWriter(absolutePath);
@@ -44,47 +45,52 @@ public class DataText {
 				
 			fileWriter.close();
 		}
+		//catches errors
 		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+	//adds parameters to the given array list
 	public void updateArray(ArrayList<String> saveFile, String title, String name, String ID) {
 		
 		saveFile.add(title + "," + name + "," + ID);
 		
 	}
-	
+	//Converts text in textfile to strings in a given arraylist
 	public ArrayList<String> textToArray(String fileName, String directory) {
-		
+		//gets current working directory
 		String absolutePath = directory + File.separator + fileName;
 		
 		ArrayList<String> ret = new ArrayList<String>();
-		
+		//reads text as string array into arraylist
 		try {
 			ret = (ArrayList<String>) Files.readAllLines(Paths.get(absolutePath));
+			//test cases
 			System.out.println(ret.size());
 			System.out.println(ret.get(3));
 			
 			return ret;
 		}
+		//catches errors
 		catch (IOException e) {
 			System.out.print("Failed");
 		}
 		return ret;
 	}
-	
+	//converts arraylist elements to text in given text file
 	public void arrayToText (ArrayList<String> arrayName, String fileName, String directory ) {
-		
+		//gets current working directory
 		String absolutePath = directory + File.separator + fileName;
-		
+		//opens a fileWriter and writes arraylist elements to text file
 		try {
 			FileWriter writer = new FileWriter(absolutePath);
 			for (String str: arrayName) {
 				writer.write(str + System.lineSeparator());
 			}
+			//closes filewriter
 			writer.close();
 		}
+		//catches exceptions
 		catch (IOException e) {
 			System.out.print("Failed");
 		}
